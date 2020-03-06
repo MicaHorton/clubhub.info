47b323aaa66d0f4a5f9e7b746f2af8cb49b3898a
Mica Horton <mica.e.horton@gmail.com>
2020-03-05 18:50:33 -0800

    Fix selection algorithim bug
    close: #1

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,43 +1,41 @@
 //Show Club Index 
 const setupInfo = (data) => {
     const clubIndex = document.querySelector('.clubIndex'); //Using querySelectorAll doesn't work for some reason...
-     //Add A List Element to HTML for Each Club
+    //Add A List Element to HTML for Each Club
     data.forEach(doc => {
         //document (refers to window element) != doc (refers to piece of data in Firestore database)
         let newLi = document.createElement('li');
-
         clubInfo = doc.data();
         newLi.innerHTML = clubInfo.club;
-        newLi.id = doc.id;
-        
-        clubData = data;
-        newLi.setAttribute('onclick', 'showPage(clubData, newLi.id)');
 
+        newLi.id = doc.id;
+        newLi.setAttribute('onclick', 'showPage()');
 
         clubIndex.appendChild(newLi);
     });
 }; 
 
-//Show Indvidual Club Page
-function showPage (data, chosenClub) {
+//Show Individual Page When Clicked
+function showPage () {
     //Hides Club Index
     const clubIndex = document.querySelector('.clubIndex');
     clubIndex.style.display = 'none';
 
-    
-    //Displays Current Club's Info
+    //Displays Current Club's Info 
+    const clubId = event.target.id;
     const clubPage = document.querySelector('.clubPage');
-    console.log(chosenClub);
-    //const selectClub = z
 
-    /* 
-    let html = `
-        <h2>${selectClub.club}</h2>
-        <p>${selectClub.content}</p>
-    `;
-    clubPage.innerHTML = html; */
+    db.collection('info').doc(clubId).get().then(doc => {
+        let selectClub = doc.data()
+        let html = `
+            <h2>${selectClub.club}</h2>
+            <p>${selectClub.content}</p>
+        `;
+        clubPage.innerHTML = html;
 
-}
+    });
+    
+};
 
 //Get Data and Execute Code (when DOM has loaded)
 document.addEventListener('DOMContentLoaded', (e) => {
@@ -46,3 +44,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
     }); 
 });
+
