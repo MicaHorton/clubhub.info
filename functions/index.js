612735603cb44183c078184c9d77f1e8cd8315c0
Mica Horton <mica.e.horton@gmail.com>
2020-03-10 20:43:12 -0700

    Messed with calendar somemore
    Just trying to clear working tree because maybe git is causing issues
    
    See: #4

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 //Send Welcome Email
-//Setup Dependencies
+//Setup Stuff
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const fs=require('fs'); 
@@ -41,121 +41,40 @@ try {
 return null; 
 });
 
+
+
 //Add Calendar Event Function
-
-
-//Setup Dependencies
+//Setup Stuff
 const {google} = require('googleapis');
-const OAuth2 = google.auth.OAuth2; 
 const calendar = google.calendar('v3');
-const functions = require('firebase-functions');
-
+//Credentials To Service Acount
 const googleCredentials = require('./credentials.json');
 
-const ERROR_RESPONSE = {
-    status: "500",
-    message: "There was an error adding an event to your Google calendar"
-};
-const TIME_ZONE = 'EST';
-
-//Create Event 
-function addEvent(event, auth) {
-  return new Promise(function(resolve, reject) {
-      calendar.events.insert({
-          auth: auth,
-          calendarId: 'primary',
-          resource: {
-              'summary': event.eventName,
-              'description': event.description,
-              'start': {
-                  'dateTime': event.startTime,
-                  'timeZone': TIME_ZONE,
-              },
-              'end': {
-                  'dateTime': event.endTime,
-                  'timeZone': TIME_ZONE,
-              },
-          },
-      }, (err, res) => {
-          if (err) {
-              console.log('Rejecting because of error');
-              reject(err);
-          }
-          console.log('Request successful');
-          resolve(res.data);
-      });
-  });
-}
-
-//Add Event To Calendar
-exports.addEventToCalendar = functions.https.onRequest((request, response) => {
-  const eventData = {
-      eventName: request.body.eventName,
-      description: request.body.description,
-      startTime: request.body.startTime,
-      endTime: request.body.endTime
-  };
-  const oAuth2Client = new OAuth2(
-      googleCredentials.web.client_id,
-      googleCredentials.web.client_secret,
-      googleCredentials.web.redirect_uris[0]
-  );
-
-  oAuth2Client.setCredentials({
-      refresh_token: googleCredentials.refresh_token
-  });
-
-  addEvent(eventData, oAuth2Client).then(data => {
-      response.status(200).send(data);
-      return;
-  }).catch(err => {
-      console.error('Error adding event: ' + err.message); 
-      response.status(500).send(ERROR_RESPONSE); 
-      return;
-  });
-}); */
 
 
-//Sends Notification When Someone Uses Contact Form (doesn't work)
-/*
-contactNotification = bfunctions.https.onCall(
-  console.log('Firebase realized this happened!')
-  const mailOptions = {
-      from: '"ClubHub" <clubhub.lahs@gmail.com>',
-      to: '"ClubHub" <clubhub.lahs@gmail.com>',
-      subject: 'Someone contacted you!'
-  };
-
-  try {
-    mailTransport.sendMail(mailOptions);
-    console.log('mail sent');
-    
-  } catch(error) {
-    console.error('There was an error while sending the email:', error);
-  }
-  return null;
-
-); */
-
-/*
-exports.contactNotification = functions.database.instance('contacts').ref('Users/mica').onCreate((snapshot, context) => {
-  console.log('Firebase realized this happened!')
-  const mailOptions = {
-      from: '"ClubHub" <clubhub.lahs@gmail.com>',
-      to: '"ClubHub" <clubhub.lahs@gmail.com>',
-      subject: 'Someone contacted you!'
-  };
-  
-  try {
-    mailTransport.sendMail(mailOptions);
-    console.log('mail sent');
-    
-  } catch(error) {
-    console.error('There was an error while sending the email:', error);
-  }
-  return null; 
-
-});
-*/
-
-
+//Actual Function
+function addEvent(event) {
+return new Promise(function(resolve, reject) {
+    calendar.events.insert({
+        calendarId: 'primary',
+        resource: {
+            'summary': event.eventName,
+            'description': event.description,
+            'start': {
+                'dateTime': event.startTime,
+                'timeZone': TIME_ZONE,
+            },
+            'end': {
+                'dateTime': event.endTime,
+                'timeZone': TIME_ZONE,
+            },
+        },
+    }, (err, res) => {
+        if (err) {
+            console.log('Rejecting because of error');
+            reject(err);
+        }
+        console.log('Request successful');
+        resolve(res.data);
+    });
+}); 
