1fd4e7236e0b3babf1a19b6f7288cd0d3e87edc8
Mica Horton <mica.e.horton@gmail.com>
2020-03-10 19:48:54 -0700

    "Messed with calendar stuff"
    See: #4

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-//SetUp Email Stuff
+//Send Welcome Email
+//Setup Dependencies
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const fs=require('fs'); 
@@ -16,10 +17,10 @@ const mailTransport = nodemailer.createTransport({
   },
 });
 
-//Get Welcome Email Information
+//Get Content of Welcome Email 
 var htmlmail=fs.readFileSync("welcome.html","utf-8").toString();
 
-//Send Welcome Email
+//Actual Welcome Email Functon
 exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
   const recipent_email = user.email; 
  
@@ -39,6 +40,81 @@ try {
 }
 return null; 
 });
+
+//Add Calendar Event Function
+
+
+//Setup Dependencies
+const {google} = require('googleapis');
+const OAuth2 = google.auth.OAuth2; 
+const calendar = google.calendar('v3');
+const functions = require('firebase-functions');
+
+const googleCredentials = require('./credentials.json');
+
+const ERROR_RESPONSE = {
+    status: "500",
+    message: "There was an error adding an event to your Google calendar"
+};
+const TIME_ZONE = 'EST';
+
+//Create Event 
+function addEvent(event, auth) {
+  return new Promise(function(resolve, reject) {
+      calendar.events.insert({
+          auth: auth,
+          calendarId: 'primary',
+          resource: {
+              'summary': event.eventName,
+              'description': event.description,
+              'start': {
+                  'dateTime': event.startTime,
+                  'timeZone': TIME_ZONE,
+              },
+              'end': {
+                  'dateTime': event.endTime,
+                  'timeZone': TIME_ZONE,
+              },
+          },
+      }, (err, res) => {
+          if (err) {
+              console.log('Rejecting because of error');
+              reject(err);
+          }
+          console.log('Request successful');
+          resolve(res.data);
+      });
+  });
+}
+
+//Add Event To Calendar
+exports.addEventToCalendar = functions.https.onRequest((request, response) => {
+  const eventData = {
+      eventName: request.body.eventName,
+      description: request.body.description,
+      startTime: request.body.startTime,
+      endTime: request.body.endTime
+  };
+  const oAuth2Client = new OAuth2(
+      googleCredentials.web.client_id,
+      googleCredentials.web.client_secret,
+      googleCredentials.web.redirect_uris[0]
+  );
+
+  oAuth2Client.setCredentials({
+      refresh_token: googleCredentials.refresh_token
+  });
+
+  addEvent(eventData, oAuth2Client).then(data => {
+      response.status(200).send(data);
+      return;
+  }).catch(err => {
+      console.error('Error adding event: ' + err.message); 
+      response.status(500).send(ERROR_RESPONSE); 
+      return;
+  });
+}); */
+
 
 //Sends Notification When Someone Uses Contact Form (doesn't work)
 /*
